ed9f5e31c38da29c30d724032599939b2f09efaf
Jacopo <jacopoaugelli04@gmail.com>
2026-05-12 14:42:28 +0200

    secondo tentativo dello svolgimento dell'esercizio

--- script.js
+++ script.js
@@ -4,47 +4,29 @@ const senior = "thirdCategory (sconto 40%)";
 const standard = "secondCategory";
 
 
-const getRand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
 
 const prezzoAlKm = 0.21;
 const scontoChild = 20;
 const scontoSenior = 40;
 
 
-function assegnaCategoria(età) {
-    if (età < 18){
-        return child;
-    } else if (età >= 66) {
-        return senior;
-    } else {
-        return standard;
-    }
+
+let età = prompt("quanti anni hai?")
+let KmDaPercorrere = prompt("quanti Km devi percorrere")
+let prezzoLordo = (prezzoAlKm * KmDaPercorrere)
+
+if (età < 18) {
+   prezzoFinale = (prezzoLordo - (prezzoLordo * 0.2))
+
+} else if (età > 65) {
+
+    prezzoFinale = (prezzoLordo - (prezzoLordo * 0.4))
 }
 
-function identificaPrezzoBiglietto(km, età){
 
-    let prezzoBase = km * prezzoAlKm;
-    let prezzoFinale = prezzoBase;
 
-    if (età < 18) {
-        prezzoFinale = prezzoBase - (prezzoBase * scontoChild / 100);
-    } else if (età >= 66) {
-        prezzoFinale = prezzoBase - (prezzoBase * scontoSenior / 100);
-    } 
-        return prezzoFinale;
-    
-}
-
-let etàUtente = getRand(5, 90);
-let KmDaPercorrere = getRand(10, 500);
-
-let categoriaOttenuta = assegnaCategoria(etàUtente);
-let prezzoFinale = identificaPrezzoBiglietto(KmDaPercorrere, etàUtente);
-
-console.log("Età passeggero: " + etàUtente);
+console.log("Età passeggero: " + età);
 console.log("Km da percorrere: " + KmDaPercorrere);
-console.log("Categoria: " + categoriaOttenuta);
 console.log("Prezzo totale: " + prezzoFinale.toFixed(2) + "€");
 
 
