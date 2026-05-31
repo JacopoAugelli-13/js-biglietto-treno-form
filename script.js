5755ccd24b6a9f22fc8b22d17726b460ad7ef444
Jacopo <jacopoaugelli04@gmail.com>
2026-05-31 14:40:40 +0200

    prime due MILESTONE eseguite

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 
-const child = "firstCategory (sconto 20%)";
-const senior = "thirdCategory (sconto 40%)";
-const standard = "secondCategory";
-
-
-
 const prezzoAlKm = 0.21;
 const scontoChild = 20;
 const scontoSenior = 40;
 
+const Name = document.querySelector('.name');
+const surname = document.querySelector('.surname');
+const Età = document.querySelector('.età');
+const Km = document.querySelector('.km');
 
 
-let età = prompt("quanti anni hai?")
-let KmDaPercorrere = prompt("quanti Km devi percorrere")
-let prezzoLordo = (prezzoAlKm * KmDaPercorrere)
+let button = document.querySelector('#calcolatore');
+button.addEventListener('click', () => {
+    let prezzoFinale;
+    let eta = Età.valueAsNumber;
+    let KmDaPercorrere = Km.valueAsNumber;
+    let prezzoLordo = (prezzoAlKm * KmDaPercorrere)
 
-if (età < 18) {
-   prezzoFinale = (prezzoLordo - (prezzoLordo * 0.2))
+    if (eta < 18) {
+        prezzoFinale = (prezzoLordo - (prezzoLordo * 0.2));
 
-} else if (età > 65) {
+    } else if (eta > 65) {
 
-    prezzoFinale = (prezzoLordo - (prezzoLordo * 0.4))
-}
+        prezzoFinale = (prezzoLordo - (prezzoLordo * 0.4));
+    } else {
 
+        prezzoFinale = prezzoLordo;
+    }
 
-
-console.log("Età passeggero: " + età);
-console.log("Km da percorrere: " + KmDaPercorrere);
-console.log("Prezzo totale: " + prezzoFinale.toFixed(2) + "€");
+    let risultato = document.getElementById('risultato')
+    risultato.innerHTML = prezzoFinale + "€";
+    console.log("Età passeggero: " + eta);
+    console.log("Km da percorrere: " + KmDaPercorrere);
+    console.log("Prezzo totale: " + prezzoFinale.toFixed(2) + "€");
+})
 
 
 
